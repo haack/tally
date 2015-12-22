187210408cfbff1b995d41e20cf4884c1bfc2263
Alex Haak <ahaak@guidespark.com>
2015-12-21 23:27:30 -0800

    Added poll seed with lodash map

--- app/scripts/ui/Feed.js
+++ app/scripts/ui/Feed.js
@@ -1,17 +1,34 @@
 'use strict';
 
+var _ = require('lodash');
+
 var Feed = React.createClass({
   getInitialState: function() {
     return {
-      polls: [1,2,3,4]
+      polls: [
+        {
+          question_string: "Wagwan?",
+          options: {
+            yes: {count:123},
+            no: {count:321}
+          }
+        },
+        {
+          question_string: "Hello?",
+          options: {
+            yes: {count:123},
+            no: {count:321}
+          }
+        }
+      ]
     };
   },
 
   render: function() {
     var createPoll = function(pollObject, i) {
-      return <li key={i}>{pollObject}</li>;
+      return <li key={i}>{pollObject.question_string}</li>;
     };
-    return <ul>{this.state.polls.map(createPoll)}</ul>;
+    return <ul>{_.map(this.state.polls, function(value, key) {return createPoll(value, key)})}</ul>;
   }
 });
 
