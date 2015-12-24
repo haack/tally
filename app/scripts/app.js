a439cfbe52001ab0aa1243f812dc9dc69c8d8a4c
Alex Haak <ahaak@guidespark.com>
2015-12-23 16:47:20 -0800

    Change file structure to reflect "routes"

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,18 @@
 
 var React = window.React = require('react'),
     ReactDOM = require("react-dom"),
-    Feed = require("./ui/Feed"),
+    Feed = require("./ui/Feed/Feed"),
+    NewPoll = require("./ui/NewPoll/NewPoll"),
     mountNode = document.getElementById("app");
 
 var TallyApp = React.createClass({
   render: function() {
-    return <Feed/>;
+    return (
+      <div>
+        <Feed/>
+        <NewPoll/>
+      </div>
+      );
   }
 });
 
---
+++ app/scripts/ui/Example.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var Example = React.createClass({
+  render: function() {
+    return (
+      <div>Content</div>
+    );
+  },
+});
+
+module.exports = Example;
