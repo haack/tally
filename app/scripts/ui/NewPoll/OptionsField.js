d7c88351a993ecdba3a996d7d6ed03eb81cc5a58
Alex Haak <ahaak@guidespark.com>
2015-12-27 12:40:07 -0800

    Added option add and remove

--- app/scripts/ui/NewPoll/OptionsField.js
+++ app/scripts/ui/NewPoll/OptionsField.js
@@ -1,13 +1,28 @@
 'use strict';
 
-var OptionsField = React.createClass({
-  handleOptionAdd: function(event) {
+var _ = require('lodash');
 
+var OptionsField = React.createClass({
+  addOption: function(event) {
+    var options = this.props.formData.options;
+    options[event.target.value] = event.target.value;
+    this.props.saveValues({options: options});
+  },
+
+  removeOption: function(event) {
+    this.props.saveValues({options: _.omit(this.props.formData.options, event.target.value)});
   },
 
   render: function() {
     return (
       <div>
+        <ol>
+          {_.map(this.props.formData.options, function(val, key) {
+            return <li key={key}>{key}<button value={key} onClick={this.removeOption}>-</button></li>;
+          }.bind(this))}
+          <button value="yes" onClick={this.addOption}>+</button>
+        </ol>
+
         <button onClick={this.props.prevStep}>Prev</button>
         <button onClick={this.props.nextStep}>Next</button>
       </div>
