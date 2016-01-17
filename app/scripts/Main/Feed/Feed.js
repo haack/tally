cff9f3edd43488677ce2fb035df30e57c6331860
Alex Haak <ahaak@guidespark.com>
2016-01-16 18:00:21 -0800

    Cleanup

--- app/scripts/Main/Feed/Feed.js
+++ app/scripts/Main/Feed/Feed.js
@@ -15,32 +15,16 @@ var Feed = React.createClass({
   },
 
   componentWillMount: function() {
-    // firebaseRef.on("value", function(dataSnapshot) {
-    //   this.setState({
-    //     poll: dataSnapshot.val(),
-    //     count: dataSnapshot.numChildren()
-    //   });
-    // }.bind(this));
-
     firebaseRef.orderByChild("votes").on("child_added", function(data) {
       this.state.polls.unshift(data.key()) //push to bottom
       this.setState({
         polls: this.state.polls
       });
     }.bind(this));
-
-    // firebaseRef.orderByChild("votes").on("child_added", function(snapshot) {
-    //   console.log(snapshot.key() + " was " + snapshot.val().votes + " meters tall");
-    //   this.setState({
-    //     polls: _.assign(this.state.polls, {[snapshot.key()]: snapshot.val()}),
-    //     count: this.state.count+1
-    //   });
-    // }.bind(this));
   },
 
   render: function() {
     var createPoll = function(i) {
-      console.log(i);
       return <Poll key={i} pollRef={firebaseRef.child(i)} />;
     };
     if (this.state.count > 0) {
