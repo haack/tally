22f97311c2db0542e1e0ac750c059152651474cf
Alex Haak <ahaak@guidespark.com>
2016-01-21 16:50:49 -0800

    Cleanup styles

--- app/scripts/Main/AddButton.js
+++ app/scripts/Main/AddButton.js
@@ -3,7 +3,9 @@
 var AddButton = React.createClass({
   render: function() {
     return (
-      <a href="#/add"><span className="add-button"><img className="icon" src="/images/plus.png" width="30px"></img> Create Poll</span></a>
+      <a href="#/add">
+        <span className="add-button"><img className="icon" src="/images/plus.png" width="30px"></img> Create Poll</span>
+      </a>
     );
   }
 });
