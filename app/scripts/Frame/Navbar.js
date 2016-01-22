a8722151ace3c79faf47bd488c26cf6fc8bdb5da
Alex Haak <ahaak@guidespark.com>
2016-01-21 18:39:55 -0800

    Added dropdown and home button

--- app/scripts/Frame/Navbar.js
+++ app/scripts/Frame/Navbar.js
@@ -4,9 +4,28 @@ var Navbar = React.createClass({
   render: function() {
     return (
       <div className="nav">
-        <span>Tally</span>
-        <i className="nav-options fa fa-chevron-circle-down pull-right"></i>
+        <a href="#/main">
+          <i className="nav-icon fa fa-bar-chart pull-left"></i>
+        </a>          
+        <a href="#/main">
+          <span>Tally</span>
+        </a>
+        <div className="dropdown pull-right">
+          <a type="button" id="menu1" data-toggle="dropdown">
+            <i className="nav-icon fa fa-chevron-circle-down pull-right"></i>
+          </a>
+          <ul className="dropdown-menu pull-right"aria-labelledby="menu1">
+            <li><a href="#/main">Home</a></li>
+            {
+              (!Auth.getAuth()) ? 
+                <li><a href="#/login"><i className="fa fa-sign-in"></i> Login</a></li> 
+                : 
+                <li><a href="#/logout"><i className="fa fa-sign-out"></i> Logout</a></li>
+            }
+          </ul>
+        </div>
       </div>
+
     );
   },
 });
